3c040d3eb266c92ea907afd1d835c5e8a48bae95
arlan <arlannomad@gmail.com>
2024-06-30 20:47:50 +0500

    react-frontend-adin

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,8 +30,8 @@ const Navbar = () => {
           <li><Link to='about' smooth={true} offset={-150} duration={500}>О нас</Link></li>
           <li><Link to='campus' smooth={true} offset={-260} duration={500}>Наши продукты</Link></li>
           <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Отзывы клиентов</Link></li>
-          {/* <li><a href="http://localhost:8081/"><button className='btn'>Статьи</button></a></li> */}
-          <li><a href="https://adin-fontend-list.netlify.app/"><button className='btn'>Статьи</button></a></li>
+          <li><a href="http://localhost:8081/"><button className='btn'>Статьи</button></a></li>
+          {/* <li><a href="https://adin-fontend-list.netlify.app/"><button className='btn'>Статьи</button></a></li> */}
           <li><Link Link to='contact' smooth={true} offset={-260} duration={500}
            className='btn'>Контакты</Link></li>
       </ul>
